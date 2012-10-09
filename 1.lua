a0063ab2164605f1eea6429fa3e38e54fa5822a7
Daogang Tang <daogangtang@gmail.com>
2012-10-09 22:50:07 +0800

    make this a basic file server.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- 1.lua
+++ 1.lua
@@ -1,6 +1,7 @@
 local luv = require 'luv'
 
-local t = luv.fs.stat("/tmp")
+local t = luv.fs.stat("11.jpg")
+print(t)
 for k,v in pairs(t) do
    print(k, "=>", v)
 end
--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 -- includes
 local luv = require 'luv'
 local lhp = require 'http.parser'
+local mimetypes = require 'mime'
+
 
 local HTTP_FORMAT = 'HTTP/1.1 %s %s\r\n%s\r\n\r\n%s'
 
@@ -14,9 +16,8 @@ local function http_response(body, code, status, headers)
 
     return string.format(HTTP_FORMAT, code, status, table.concat(raw, '\r\n'), body)
 end
-local function http_response2(code, status, headers)
-    headers['content-length'] = 139168
 
+local function http_response_header(code, status, headers)
     local raw = {}
     for k, v in pairs(headers) do
         table.insert(raw, string.format('%s: %s', tostring(k), tostring(v)))
@@ -25,80 +26,107 @@ local function http_response2(code, status, headers)
     return string.format(HTTP_FORMAT, code, status, table.concat(raw, '\r\n'), '')
 end
 
+local function parse_path_query_fragment(uri)
+    local path, query, fragment, off
+    -- parse path
+    path, off = uri:match('([^?]*)()')
+    -- parse query
+    if uri:sub(off, off) == '?' then
+        query, off = uri:match('([^#]*)()', off + 1)
+    end
+    -- parse fragment
+    if uri:sub(off, off) == '#' then
+        fragment = uri:sub(off + 1)
+        off = #uri
+    end
+    return path or '/', query, fragment
+end
 
-
-
+local req = nil
 function init_parser()
-   local cur          = { body = {} }
-   local cb           = {}
+   local reqs	= {}
+   local cur	= {}
+   local cb     = {}
 
    function cb.on_message_begin()
-	print('on_message_begin')
-       cur = { headers = {}, body={} }
+	cur = {headers = {}, body={}}
    end
 
-   function cb.on_url(value)
-	print('on_url', value)
-       cur.url = value;
-       --cur.path, cur.query_string, cur.fragment = parse_path_query_fragment(value)
-   end
-
-   function cb.on_body(value)
-	print('on_body', value)
-       table.insert(cur.body, value)
+   function cb.on_url(url)
+	print(os.date("%Y-%m-%d %H:%M:%S", os.time()), 'request ', url)
+	cur.url = url
+	cur.path, cur.query_string, cur.fragment = parse_path_query_fragment(url)
    end
 
    function cb.on_header(field, value)
-	print('on_header', value)
        cur.headers[field] = value
    end
 
+   function cb.on_body(body)
+--	if ( nil == cur.body ) then
+--		cur.body = {}
+--	end
+	table.insert(cur.body, body)
+   end
+   
    function cb.on_message_complete()
-	print('on_message_complete', value)
-       --table.insert(reqs, cur)
-       cur = {body={}}
+	--table.remove(reqs)
+	--table.insert(reqs, cur)
+	req = cur
+	cur = {headers = {}, body={}}
    end
 
    return lhp.request(cb)
 end
 
+
 local parser = init_parser()
 
 
-
-function feedfile(client)
-local f1 = luv.fiber.create(function()
-   print("enter file read")
-   local file = luv.fs.open("1.jpg", "r", "664")
-print('file', file)
-local res = http_response2(200, 'OK', {['content-type'] = 'image/jpeg'})
-client:write(res)
-while true do
-local nread, content = file:read(4096)	
---print(content, #content)
---client:write(content)
-if nread then
-client:write(content)
-else
-break
-end
+function findtype(req)
+	local content_type
+	-- now req is the incoming request data
+	local ext = req.path:match('(%.%w+)$')
+	if ext then
+		content_type = mimetypes[ext]
+	end
+	req['content-type'] = content_type or 'text/plain'
 end
 
-print("close:", file:close())
---client:close()
-end)
 
-f1:ready()
---f1:join()
+function feedfile(client, req)
+	local f1 = luv.fiber.create(function()
+		local path = '.'..req.path
+		local file_t = luv.fs.stat(path)
+		local size = 0
+		if file_t then size = file_t.size end
+		local file = luv.fs.open(path, "r", "664")
+		--print(file_t)
+		if not file_t then
+			client:write(http_response('Not Found', 404, 'Not Found', {
+				['content-type'] = 'text/plain'
+			}))
+		else
+			local res = http_response_header(200, 'OK', {
+				['content-type'] = req['content-type'],
+				['content-length'] = size
+			})
+			client:write(res)
+			while true do
+				local nread, content = file:read(4096)	
+				if nread then
+					client:write(content)
+				else
+					break
+				end
+			end
+
+			file:close()
+		end
+	end)
+
+	f1:ready()
 end
-
---local file = luv.fs.open("/tmp/cheese.ric", "r", "664")
---print("READ:", file:read())
---file:close()
---print("DELETE:", luv.fs.unlink("/tmp/cheese.ric"))
-
-
-
 
 
 local main = luv.fiber.create(function()
@@ -114,20 +142,16 @@ local main = luv.fiber.create(function()
       local child = luv.fiber.create(function()
          while true do
             local got, str = client:read()
-	-- print('->got', got)
             if got then
 		-- print(str)
             	local bytes_read = parser:execute(str)
-		print('bytes_read', bytes_read)
-		--local res = http_response('Hello world!', 200, 'OK', {})
-               	feedfile(client)
-		--client:close()
-		-- client:write(res)
-		--client:close()
-		--break
+		-- print('bytes_read', bytes_read)
+		if bytes_read > 0 then
+			feedfile(client, req)
+		end
             else
-               client:close()
-               break
+		client:close()
+		break
             end
          end
       end)
