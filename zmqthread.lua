8e70d149d80ce32e0fc0ca93f1de76fadfb9462f
Daogang Tang <daogangtang@gmail.com>
2012-11-26 12:25:03 +0800

    make zip file hieriarchy.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- zmqthread.lua
+++ zmqthread.lua
@@ -14,11 +14,12 @@ return
     local ctx = zmq.init(1)
     local channel_sub = ctx:socket(zmq.SUB)
 	channel_sub:setopt(zmq.SUBSCRIBE, "")
-	channel_sub:connect(channel_sub_addr)
+--	channel_sub:connect(channel_sub_addr)
+	channel_sub:bind(channel_sub_addr)
 		
     while true do
 		local msg, err = channel_sub:recv()   -- block wait
-		-- print('return msg...', #msg)
+		-- print('return msg...', msg, #msg)
 
 		local s, errmsg = client:send(#msg..' '..msg)
 		if not s and errmsg == 'closed' then
